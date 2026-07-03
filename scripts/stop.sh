d0413840626acd386c7811fd88085912bac51e6d
TirthMesariya <tirthmesariya9638@gmail.com>
2026-07-03 12:41:17 +0530

    Add stop script for Wanderlust Application

---
+++ scripts/stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "======================================"
+echo "Stopping Wanderlust Application..."
+echo "======================================"
+
+docker compose down
+
+echo ""
+echo "Containers Stopped Successfully!"
